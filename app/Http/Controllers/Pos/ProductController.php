fdb459a0297ceac5d21925d7928e42f211cc63be
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-10-08 16:05:55 +0700

    Product update

--- app/Http/Controllers/Pos/ProductController.php
+++ app/Http/Controllers/Pos/ProductController.php
@@ -42,7 +42,45 @@ class ProductController extends Controller
             'created_at' => Carbon::now(),
         ]);
 
-        return redirect()->route('product.all');
+        $notification = array(
+            'message' => 'เพิ่มสินค้าเรียบร้อยแล้ว', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('product.all')->with($notification);
 
     } // end method
+
+    public function ProductEdit($id){
+
+        $unit = Unit::all();
+        $category = Category::all();
+        $supplier = Supplier::all();
+        $product = Product::findOrFail($id);
+        return view('backend.product.product_edit',compact('unit','category','supplier','product'));
+    } // end method
+
+    public function ProductUpdate(Request $request){
+
+        $product_id = $request->id;
+
+         Product::findOrFail($product_id)->update([
+
+            'name' => $request->name,
+            'supplier_id' => $request->supplier_id,
+            'unit_id' => $request->unit_id,
+            'category_id' => $request->category_id, 
+            'updated_by' => Auth::user()->id,
+            'updated_at' => Carbon::now(), 
+        ]);
+
+        $notification = array(
+            'message' => 'อัพเดทสินค้าเรียบร้อยแล้ว', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('product.all')->with($notification); 
+
+
+    } // End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -125,6 +125,8 @@ Route::controller(ProductController::class)->group(function(){
     Route::get('/product/add', 'ProductAdd')->name('product.add');
 
     Route::post('/product/store', 'ProductStore')->name('product.store');
+    Route::get('/product/edit/{id}', 'ProductEdit')->name('product.edit');
+    Route::post('/product/update', 'ProductUpdate')->name('product.update');
     
    
     
