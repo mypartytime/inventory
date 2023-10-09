14a02fb052463635121ac00c1dc6d85c1206b358
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-10-09 16:45:43 +0700

    fix delete

--- resources/views/backend/invoice/invoice_pending_list.blade.php
+++ resources/views/backend/invoice/invoice_pending_list.blade.php
@@ -65,9 +65,9 @@
 
       <td>
        @if($item->status == '0')
- <a href="{{ route('invoice.delete',$item->id) }}" class="btn btn-dark sm" title="Approved Data" >  <i class="fas fa-check-circle"></i> </a>
+ <a href="" class="btn btn-dark sm" title="Approved Data" >  <i class="fas fa-check-circle"></i> </a>
 
-<a href="{{ route('purchase.delete',$item->id) }}" class="btn btn-danger sm" title="Delete Data" id="delete">  <i class="fas fa-trash-alt"></i> </a>
+<a href="{{ route('invoice.delete',$item->id) }}" class="btn btn-danger sm" title="Delete Data" id="delete">  <i class="fas fa-trash-alt"></i> </a>
 @endif  
 </td>
 
